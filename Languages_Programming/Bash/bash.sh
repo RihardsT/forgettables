c478c92bb29da442b9ab72ebf6894e51c6a38f74
Rihards Teteris <richitislv@gmail.com>
2017-05-27 21:18:26 +0300

    Beautify bash

--- Languages_Programming/Bash/bash.sh
+++ Languages_Programming/Bash/bash.sh
@@ -1,3 +1,21 @@
+######################## Comments ##############################################
+# Simple pimple comments with #
+
+######################## Print #################################################
+echo "This stuff"
+
+######################## Operators #############################################
+######################## Comparators ###########################################
+
+######################## Variables #############################################
+# Get file into array. Line by line. Without the cut, array[0] is the whole file
+# Would be nice, if I'd find a proper newline delimiter here
+subjects=( $(cat subjects| cut -d' ' -f1) )
+${arrayName[0]} # This is how to access the item
+
+
+
+######################## if else ##############################################
 # man test # for expressions usable in if statements
 # -z to check if variable is empty.
 if [ -z ${MYWLANIP} ]; then echo "Yep"; fi
@@ -11,12 +29,17 @@ then
 else
   echo "nay"
 fi
-
 # If string equals a string
 if [ $(uname -m) = "x86_64" ]; then echo "Yep"; fi
 
 
+
+
+
+######################## Loops ################################################
 for i in "${arrayName[@]}"
 do
   echo $i
 done
+
+######################## Functions #############################################
