f0013f2eb9cb58bfcabafc4e48fb272099cf148a
Rihards Teteris <richitislv@gmail.com>
2017-05-29 19:22:30 +0300

    Some Matlab stuff

--- Languages_Programming/Matlab/matlab.m
+++ Languages_Programming/Matlab/matlab.m
@@ -1,29 +1,55 @@
-%% Comments
+%% Comments %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % Comments
   %% Section separator
 
-%% Operators:
-%% Comparators:
-%% print
-%% Variables:
-Struct{1} = something
+%% Operators: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Comparators: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+< <= > >=
+~ % ~ == NOT
+%% print %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Use ; to not output the value to console
+a = 'filename';
+['folder/' a '.mat'] % String interpolation, one way
+b = 12
+['folder/' num2str(b) '.mat']
+
+name = 'Alice'; age = 12;
+X = sprintf('%s will be %d this year.',name,age); % String interpolation
+disp(X)
+
+%% Variables: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Struct{1} = something;
+Struct{1}
 Struct.something
 
+labels(1:3,1) = {'Value in cell'}; % Add values to cell in a range
 
-%% if/else:
+% Linear index
+A = [ 1 2;
+      3 4]
+sub2ind(size(A),1,2) % Find the linear index. Line, col.
+A(3) % == 2, same as A(1,2)
 
-%% Loops:
+% Get range of values from matrix
+A(A > 10 & A < 45)
+% Every line that has first item in range, and every column of that line
+A(A(:,1) > 10 & A(:,1) < 45, :)
+
+%% if/else: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Loops: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 for i = 1:10:100 % from:step:to
 end
 for i = [1,2,3, 5,7,9, 12,15,18] % Irregular step, can also use 1:2:3 3:3:9
   % < process using i >
+  pause(1) % sleep for 1 second
 end
 
 
-%% Functions:
+%% Functions: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%% String interpolation
+%% String interpolation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   for K=1:10
     filename = sprintf('out%d.doc', K);
     fid = fopen(filename,'w');
@@ -31,14 +57,7 @@ end
     fclose(fid);
   end
 
-  for i=1:10
-    somevalue= <something>
-    save(['GeneralPrefix' num2str(i) '.mat'],'somevalue')
-  end
-
-
-% Linear index
-A = [ 1 2;
-      3 4]
-sub2ind(size(A),1,2) % Find the linear index. Line, col.
-A(3) % == 2, same as A(1,2)
+%% Plot: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+axis([-2 2 -2 2]) % x x y y % Limit the plot range
+scatter(x,y) % Scatter
+plot(x,y,'b.') %plot, lines by default, but with 'b.' will be blue dots
