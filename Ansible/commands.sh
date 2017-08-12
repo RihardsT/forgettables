3c726cc66c851f1fe671be73c9bd79963ec4a7d4
Rihards Teteris <richitislv@gmail.com>
2017-08-12 15:23:05 +0300

    Role requirements.yml

--- Ansible/commands.sh
+++ Ansible/commands.sh
@@ -2,7 +2,9 @@ pyenv local 2.7.13 # Because ansible and molecule work better with python2
 pip install -U packaging; pip install -U ansible
 which ansible
 
-ansible-galaxy install --roles-path ./roles
+ansible-galaxy install --roles-path ./roles ANSIBLE_GALAXY_ROLE
+ansible-galaxy install --roles-path ./roles -r requirements.yml #Or put required roles in requirements
+
 # ansible-playbook # apply a playbook to servers
 # --check # dry run
 # --forks
@@ -16,6 +18,10 @@ export ANSIBLE_NOCOWS=1 # Skip the overly verbose cows
 echo "export ANSIBLE_NOCOWS=1" >> ~/.bash_profile
 
 ansible-playbook -i ./Inventory --limit backend --diff Playbook.yml
+# Pass environment variable, here set to use python3. Same can be done in Inventory file
+ansible-playbook -i ./Inventory -e 'ansible_python_interpreter=/usr/bin/python3' -u USER Playbook.yml
+# Other variable examples:
+-e 'ansible_port=22'
 
 # Run setup to get a list of all available ansible variables, like ansible_architecture
 ansible -i Inventory -m setup HOSTS
@@ -29,3 +35,9 @@ mkdir roles
 cd roles
 ansible-galaxy init common
 ansible-galaxy init apache
+
+# Adding tests with Molecule:
+pip2 install -U ansible docker molecule
+molecule init --driver docker
+  # Add ansible: to molecule.yml
+molecule --help
