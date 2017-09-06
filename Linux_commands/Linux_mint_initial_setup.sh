f51e12cf4ed9270ca1614a69090332475ac7e8aa
Rihards Teteris <richitislv@gmail.com>
2017-09-06 23:48:07 +0300

    Add some more stuff for inital install

--- Linux_commands/Linux_mint_initial_setup.sh
+++ Linux_commands/Linux_mint_initial_setup.sh
@@ -4,16 +4,28 @@ sudo su
 apt-get update
 apt-get -y upgrade
 apt-get purge -y hexchat transmission-* pidgin* xplayer* rhythmbox*
-apt-get install -y vim git chromium-browser audacious vlc
+apt-get install -y vim git chromium-browser audacious vlc keepass2
+curl -fsSL https://get.docker.com/ | sh
+sudo usermod -aG docker rihards
 
 ### Automount a disk
 lsblk -o NAME,SIZE,MOUNTPOINT,UUID
 # Get your disks UUID
 mkdir /media/1TB
 mount /dev/sda5 /media/1TB
-echo "UUID=YOUR_UUID_GOES_RIGHT_HERE /media/1TB ntfs-3g defaults 0 0" >> /etc/fstab
+UUID=SUBSTITUTE_WITH_YOUR_UUID_RIGHT_HERE_IF_YOU_WANT
+echo "UUID=${UUID} /media/1TB ntfs-3g defaults,permissions 0 0" >> /etc/fstab
 
+# Git initial config
+git config --global push.default matching
+git config --global user.email "richitislv@gmail.com"
+git config --global user.name "Rihards Teteris"
+git config --global core.editor vim
 
+cd /media/1TB
+wget https://atom.io/download/deb # Download Atom
+dpkg -i deb # Probably this works. Dunno
+wget https://go.microsoft.com/fwlink/?LinkID=760868 # Download VS Code
 
 ### AMD driver installation
 # Because Linux Mint is like ubuntu
@@ -22,10 +34,3 @@ sed -i 's/$ID/$ID_LIKE/g' amdgpu-pro-install
 
 ### Windows 10 stuff for dual boot
 # Control Panel -> Power Options -> Choose what the power buttons do
-
-
-# Git initial config
-git config --global push.default matching
-git config --global user.email "richitislv@gmail.com"
-git config --global user.name "Rihards Teteris"
-git config --global core.editor vim
