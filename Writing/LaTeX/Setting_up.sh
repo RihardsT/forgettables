672f710298f0618cb55c91632601e641b7ec4c5e
Rihards Teteris <richitislv@gmail.com>
2017-10-06 14:58:41 +0300

    LaTeX presentation example

--- Writing/LaTeX/Setting_up.sh
+++ Writing/LaTeX/Setting_up.sh
@@ -1,11 +1,16 @@
 docker run --rm -ti debian bash
 
-apt-get update
-# Miktex or TeXLive, texmaker
-apt-get install -y vim texlive-full latexmk
+# Miktex or TeXLive, texmaker/texworks
+apt-get update && apt-get install -y texlive-full biber latexmk make vim wget
 
 
 # Lai latviešu burti būtu, tad kompilē ar XeLaTeX
 
+# Build:
+latexmk -xelatex file.tex
 
+# With Bibliography
+latexmk -xelatex file.tex
+latexmk -bibtex file.bib
+latexmk -xelatex file.tex
 latexmk -xelatex file.tex
