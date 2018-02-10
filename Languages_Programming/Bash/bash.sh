10a099bf268699326453731f7979fb5595ccc4ca
Rihards Teteris <richitislv@gmail.com>
2018-02-10 19:28:16 +0200

    Fun bash stuff

--- Languages_Programming/Bash/bash.sh
+++ Languages_Programming/Bash/bash.sh
@@ -5,18 +5,69 @@
 echo "This stuff"
 
 ######################## Operators #############################################
-%
-! && ||
+! # Negate. [ ! false ]
+&& # AND operator. Same as -a
+|| # OR operator. Same as -o
+
+= # String equality operatot. [ "abc" = "ad" ]. Also assignment for vars, lel
+!= # Not euqal [ $a = $b ]
+-z # If string is of size 0 [ -z "" ]; echo $?
+-n # # If string is not of size 0 [ -z " " ]; echo $?
+[ $a ] # Same as -n ? Checks if string is not empty
 
 # Arithmetic evaluation:
 $(( )) #http://www.gnu.org/software/bash/manual/bashref.html#Arithmetic-Expansion
 
+## Arithmetic operators with expr
++ - \* / %
+expr $a + $b
+
+### File operators
+-b FILE # Check if file is block special file [ -b $FILE ]
+-c FILE # if character special file
+-d FILE # if file is directory
+-f FILE # if ordinary file
+-g FILE # if group ID (SGID) bit is set
+-k FILE # if sticky bit is set
+-p FILE # if file is name pipe
+-t FILE # if file descriptior is open and associated with terminal
+-u FILE # if user ID (SUID) bit is set
+-r FILE # if reeadable
+-w FILE # if writeable
+-x FILE # if executable
+-s FILE # if size greather than 0
+-e FILE # if file exists
+
+
 ######################## Comparators ###########################################
 # Generaully use test for comparing variables. man test
--gt
--eq
+-eq # Equals
+-ne # Not Equals
+-gt # a greather than b
+-lt # a less than b
+-ge # a greater or euqals b
+-le # a less or euqals b
+[ $a -eq $b ] # This is how to use them. Note the spaces
+[ $a == $b ] # check equality
+[ $a != $b ]
+
 
 ######################## Variables #############################################
+# Variable names can't start with number. Can start with a letter or _
+VARIABLE="Make it read only after assignment with readonly command"
+readonly VARIABLE
+unset VARIABLE # Remove var from shell. Can't unset readonly variables
+
+### Special Variables
+$0 # Current script filename
+$n # Arguments that are passed to script. $1 $2
+$# # Numeber of args passed to script
+$* # All args as single string ? for TOKEN in $*; do echo $TOKEN; done
+$@ # All args as array of strings?
+$? # Exit status of last command
+$$ # PID of current shell
+$! # PID number of last bacground command
+
 # Get file into array. Line by line. Without the cut, array[0] is the whole file
 # Would be nice, if I'd find a proper newline delimiter here
 subjects=( $(cat subjects| cut -d' ' -f1) )
@@ -25,7 +76,8 @@ arrayName=(Value1 Value2 Value3 .. ValueN)
 ${arrayName[0]} # This is how to access the item
 ${#arrayName[*]} # Array size
 
-
+${array_name[*]} # Get all array items at once
+${array_name[@]}
 
 
 ######################## if else ##############################################
@@ -52,10 +104,17 @@ fi
 if [ $(uname -m) = "x86_64" ]; then echo "Yep"; fi
 
 
-
-
+# case
+case word in
+  pattern )
+    ;;
+esac
 
 ######################## Loops ################################################
+# Can use break and continue keywoards
+# continue breaks out of current iteration. Will skip code below continue to go to next iteration
+# break goes out whole loop
+
 for i in "${arrayName[@]}"
 do
   echo $i
@@ -77,6 +136,38 @@ echo "Array items and indexes:"
 for index in ${!array[*]}
 do
     printf "%4d: %s\n" $index ${array[$index]}
+done
+
+# Example of continue keywoard
+NUMS="1 2 3 4 5 6 7"
+for NUM in $NUMS
+do
+   Q=`expr $NUM % 2`
+   if [ $Q -eq 0 ]
+   then
+      echo "Number is an even number!!"
+      continue
+   fi
+   echo "Found odd number"
+done
+
+# While
+a=0
+while [ "$a" -lt 10 ]
+do
+   a=`expr $a + 1`
+   echo $a
+   if [ $a -eq 5 ]; then
+      break
+   fi
+done
+
+# until
+a=10
+until [ $a -lt 10 ] # Infinite loop because of condition
+do
+   echo $a
+   a=`expr $a + 1`
 done
 
 ######################## Functions #############################################
