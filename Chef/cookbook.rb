a066cd093c2efb0f3abe26e459973c89e70a58d4
Rihards Teteris <richitislv@gmail.com>
2018-03-15 17:55:02 +0200

    Chef - override template

---
+++ Chef/cookbook.rb
@@ -0,0 +1,8 @@
+# Override template in wrapper cookbook
+begin
+  res = resources(template: "FULL_PATH_TO_FILE_ON_OS_AFTER_CHEF_RUN")
+  res.cookbook "CURRENT_WRAPPER_COOKBOOK"
+  res.source "TEMPLATE_NAME_IN_WRAPPER_COOKBOOK"
+rescue Chef::Exceptions::ResourceNotFound
+  Chef::Log.warn "File not found""
+end
