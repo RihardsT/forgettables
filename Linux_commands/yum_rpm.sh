5cb3c6fe1e8e689bc29235da8cb5a5c94127b7ae
Rihards Teteris <richitislv@gmail.com>
2018-09-05 17:43:11 +0300

    Yum and rpm stuff

---
+++ Linux_commands/yum_rpm.sh
@@ -0,0 +1,50 @@
+# Show available package versions
+yum --showduplicates list PACKAGE | expand
+# Install specific version
+yum install PACKAGE-VERSION
+
+# List installed packages
+yum list installed | grep EXPRESSION
+yum list installed httpd # specific package name
+
+# Rebuild corrupted yum db
+sudo rm -f /var/lib/rpm/__db*
+sudo rpm --rebuilddb
+sudo yum clean all
+
+# Install obsolete package
+yum --setopt=obsoletes=0 install PACKAGE-VERSION
+
+
+###
+rpm -qc PACKAGE # queue (-q) package configuration (-c) files
+
+###
+yum check-update
+yum search EXPRESSION # searches description, details
+yum search all EXPRESSION # searches in package name, summary, description, details, url
+yum provides /var/www # Search what packages create the file/folder
+yum update PACKAGE
+yum remove PACKAGE
+yum list all
+yum clean all # clean cache
+
+
+
+### RPM
+yumdownloader PACKAGE # download package from yum repository
+
+rpm -ivh # -v verbose, -h progress bar
+-i # install
+-U # upgrade
+
+rpm -e PACKAGE # remove package
+
+rpm -qa # query all installed packages
+rpm -qa PACKAGE # query all, search single package
+rpm -ql PACKAGE # list files that were created by install
+
+rpm -qd PACKAGE # query documentation in RPM
+
+yum localinstall PACKAGE # also takes care of dependencies an such
+# ^^ might be preferable, rather than rpm -i
