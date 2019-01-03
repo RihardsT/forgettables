7e688765c8afd443baa66577261f8795b7e46d85
Rihards Teteris <richitislv@gmail.com>
2019-01-03 23:13:56 +0200

    User things: passwd and shadow file stuff

--- Linux_commands/user.sh
+++ Linux_commands/user.sh
@@ -4,7 +4,10 @@ adduser --disabled-password --gecos '' USERNAME # In the '' goes user descriptio
 # Add user to group
 usermod -aG GROUP_NAME USERNAME
 
-
+# Show users info, like password expiration, etc
+chage -l USERNAME
+# Remove pwd/account limit: pass -1 to option. Ex: account expiration
+chage -E -1 USERNAME
 # Change password expiry age?
 chage -I -1 -m 0 -M 99999 -E -1 USERNAME
 #
@@ -12,3 +15,21 @@ passwd -x 99999 USERNAME
 
 # user list
 less /etc/passwd
+# user password file and some other data
+less /etc/shadow
+# shadow file collumns:
+# USER:PWD_HASH:LAST_PASSWORD_DATE_SINCE_EPOCH:MIN_PWD_AGE:MAX_PWD_AGE:WARNING_DAYS:INACTIVE_DAYS:USER_EXPIRATION_DATE
+# WARNING_DAYS - how many days before password expiration to warn user
+# INACTIVE_DAYS - days in which you still can login, to change password. After it's passed, account is locked.
+# USER_EXPIRATION_DATE - date when account will be locked. Not possible to login. Basically ! before PWD_HASH in shadow file
+
+### User id's
+0 # Root user
+1-200 # system users
+201-999 # users who use system processes, but don't own files, ex apache
+
+
+
+### Defaults for new user creation?
+/etc/login.defs
+/etc/default/useradd
