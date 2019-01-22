c1606bd726acadd8f97ecf2fa9fe282b43f4e126
Rihards Teteris <richitislv@gmail.com>
2019-01-22 14:00:00 +0200

    Selinux - add context to folder, remove it and such

--- Linux_commands/Other/selinux.sh
+++ Linux_commands/Other/selinux.sh
@@ -7,3 +7,22 @@ setenforce 1
 
 ### Default config
 /etc/selinux
+
+
+# disable re-enable selinux to restore file contexts
+
+### Define context
+# Find context of existing folder. Find here the context you want maybe
+semanage fcontext -l | grep FOLDER
+# Add context -a with type -t, add to FOLDER and all contents in it (/.*),
+# it's optional context due to '?'
+semanage fcontext -a -t CONTEXT_TYPE_t '/FOLDER(/.*)?'
+
+### Restore context
+# -R recursive, -v verbose
+restorecon -Rv FOLDER
+
+### Remove context
+semanage fcontext -l | grep FOLDER
+semagage fcontext -d '/FOLDER(/.*)?'
+restorecon -Rv FOLDER
