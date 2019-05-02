8860320c768ec3fae5c98cd889352b1df6175ea2
Rihards Teteris <richitislv@gmail.com>
2019-05-02 22:24:13 +0300

    Adds more time commands

--- Linux_commands/Other/time.sh
+++ Linux_commands/Other/time.sh
@@ -2,6 +2,13 @@ timedatectl
 chronyd # on RHEL replacement for ntpd?
 
 tzselect # timezone wizard, but doesn't set it actually
+timedatectl list-timezones
 timedatectl set-timezone TIMEZONE
+
+# Set time to be local time. Disable NTP beforehand
+timedatectl set-ntp false
+timedatectl set-time HH:MM:SS
+timedatectl set-ntp true
+systemctl restart chronyd # of using chronyd instead of NTP?
 
 chronyc
