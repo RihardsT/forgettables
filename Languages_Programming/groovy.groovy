e4fa42ec79f557f901d6159ab879abf22621877e
Rihards Teteris <richitislv@gmail.com>
2019-05-03 14:06:30 +0300

    Add how jq outputs multiple values in single line. Add empty groovy reference

---
+++ Languages_Programming/groovy.groovy
@@ -0,0 +1,11 @@
+///////// Comments
+///////// Operators:
+// Ternary operator
+(EXPRESSION) ? RETURN_IF_TRUE : RETURN_IF_FALSE
+///////// Comparators:
+///////// print
+///////// Variables:
+///////// if/else:
+///////// Loops:
+///////// Functions:
+///////// Imports:
--- Linux_commands/Other/jq.sh
+++ Linux_commands/Other/jq.sh
@@ -1,3 +1,7 @@
-# raw output with -r, without quotes
+### raw output with -r, without quotes
 # select entries matching pattern
 jq -r '.[] | .name | select(match("PATTERN"))' JSON
+
+### Output reasonable formatter string, good for tables
+# Multiple values in single line
+jq -r '.[] | "\(.name) \(.another_value)"' JSON
