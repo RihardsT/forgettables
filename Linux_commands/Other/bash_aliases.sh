3420ea00b80026bd634592f93f2932fa8d2e8f08
Rihards Teteris <richitislv@gmail.com>
2019-06-29 21:47:43 +0300

    Add single bash alias for docker system prune

--- Linux_commands/Other/bash_aliases.sh
+++ Linux_commands/Other/bash_aliases.sh
@@ -1,7 +1,9 @@
 alias dps="docker ps -a --format 'table {{.ID}} | {{.Status}} | {{.Names}}'"
+alias dsp="docker system prune"
 
 
 ## Make permanent
 tee -a .bashrc <<EOF
 alias dps="docker ps -a --format 'table {{.ID}}\t{{.Status}}\t{{.Names}}'"
+alias dsp="docker system prune"
 EOF
