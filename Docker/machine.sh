b234f135102015ef79b205b22930b931bdb2c7a0
Rihards Teteris <richitislv@gmail.com>
2019-07-10 17:52:23 +0300

    Add some more docker-machine things for WSL

--- Docker/machine.sh
+++ Docker/machine.sh
@@ -9,8 +9,17 @@ docker-machine ssh default "sudo date -u $(date -u +%m%d%H%M%Y)"
 
 
 ##### Get docker working in WSL
+echo 'alias docker-machine="docker-machine.exe"' >> ~/.bashrc
 sudo ln -s /mnt/c/ /c
 # install docker
 # sudo usermod -aG docker USERNAME
+docker-machine start; docker-machine env --shell bash | sed 's/C:/\/c/' | sed 's/\\/\//g' | sed 's:#.*$::g' | sed 's/"//g'
 
-docker-machine.exe start; docker-machine.exe env --shell bash | sed 's/C:/\/c/' | sed 's/\\/\//g' | sed 's:#.*$::g' | sed 's/"//g'
+
+
+##### Errors
+### This machine has been allocated an IP address, but Docker Machine could not reach it successfully.
+# Recreate the VM
+docker-machine rm default
+docker-machine create --driver virtualbox default
+docker-machine env --shell bash | sed 's/C:/\/c/' | sed 's/\\/\//g' | sed 's:#.*$::g' | sed 's/"//g'
