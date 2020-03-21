046ee1c80cabf47752440f226e75a3a194192b76
Rihards Teteris <richitislv@gmail.com>
2020-03-21 12:10:14 +0200

    Move WSL

--- Linux_commands/wsl.sh
+++ Linux_commands/wsl.sh
@@ -1,2 +1,14 @@
 # Symlink the mounted disk to some nice place. I like this:
 ln -s /mnt/d /d
+
+
+##### Move WSL. In CMD
+wsl --list -v
+
+wsl --export Ubuntu-18.04 d:\Other\WSL.tar
+wsl --unregister Ubuntu-18.04
+wsl --import Ubuntu-18.04 d:\Other\PC\WSL d:\Other\WSL.tar
+
+# In WSL
+echo "[user]
+default=rihards" > /etc/wsl.conf
