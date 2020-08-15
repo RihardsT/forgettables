7e291e84a4d39143046f5914452d43d1ca863950
Rihards Teteris <richitislv@gmail.com>
2020-08-15 16:21:29 +0300

    Startup command with cron

--- Linux_commands/startup.sh
+++ Linux_commands/startup.sh
@@ -1,7 +1,4 @@
-### Check out /etc/rc.local file
-# Add your startup script there. Good enough?
-sudo vi /etc/rc.local
-  /bin/echo "disable" > /sys/firmware/acpi/interrupts/gpe16
-  # Had issues that kworker kacpi using lots of CPU
-  # https://www.reddit.com/r/Fedora/comments/hmmnfb/high_cpu_usage_by_kworker_kacpid_on_fedora_32/
-sudo chmod +x /etc/rc.local
+### Crontab
+@reboot ( sleep 1 ; /bin/echo "disable" > /sys/firmware/acpi/interrupts/gpe16 )
+
+`/etc/rc.local` seems to be deprecated and either seems to be more difficult to do than cron.
