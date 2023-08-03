4cd5b301af132661bfcd08de83de95a5daad238d
Rihards <rudenspavasaris@proton.me>
2023-08-03 16:16:19 +0300

    Update Linux mint setup - forgot that I had this in Ansible too

--- Linux_commands/Linux_mint_initial_setup.sh
+++ Linux_commands/Linux_mint_initial_setup.sh
@@ -31,23 +31,21 @@ mount -a
 
 /dev/sdb1 /media/1TB ntfs-3g defaults,lazytime,uid=1000,gid=1000,dmask=007,fmask=117,big_writes,nofail,x-systemd.requires=multi-user.target,auto
 
-#
-mkdir /media/nvme
-echo "/dev/nvme0n1p5 /media/nvme ext4 defaults,nofail 0 0" >> /etc/fstab
-mount -a
-
 # I have another ext4 partition
 mkdir /media/data
 echo "/dev/sdb2 /media/data ext4 defaults,nofail 0 0" >> /etc/fstab
 mount -a
 
-#
-mkdir /Code
-mkdir /home/rihards/Code
-echo "/dev/sda8 /Code ext4 defaults 0 0" | sudo tee -a /etc/fstab
+# Stac
+mkdir /media/1TB
+echo "/dev/sda5 /media/1TB ntfs-3g defaults,permissions,nofail 0 0" >> /etc/fstab
 mount -a
-chown rihards:rihards /Code
-ln -s /Code /home/rihards/Code
+mkdir /media/nvme
+echo "/dev/nvme0n1p5 /media/nvme ext4 defaults,nofail 0 0" >> /etc/fstab
+mount -a
+
+#
+ln -s /media/nvme/Code/ Code
 
 # Git initial config
 git config --global push.default matching
@@ -55,12 +53,16 @@ git config --global user.email "rudenspavasaris@proton.me"
 git config --global user.name "Rihards"
 git config --global core.editor vim
 git config --global pull.ff only
+ssh-keygen -t ed25519 -C $(cat /etc/hostname)
+cat ~/.ssh/id_ed25519.pub
 
-cd /media/1TB
-wget -O atom.deb https://atom.io/download/deb # Download Atom
-dpkg -i atom.deb
-wget -O vs_code.deb https://go.microsoft.com/fwlink/?LinkID=760868 # Download VS Code
-dpkg -i vs_code.deb
+# VSCodium
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt install codium
 
 ### Use alternate DNS server
 # https://superuser.com/questions/677343/how-to-make-name-server-address-permanent-in-etc-resolv-conf#677414
@@ -116,23 +118,25 @@ gsettings list-recursively
 
 ##### Things that can be done in Welcome screen
 ### Desktop colours
-gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-Y-Dark'
-gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark'
+gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-Y'
+gsettings set org.cinnamon.theme name 'Mint-Y-Dark'
+gsettings set org.x.apps.portal color-scheme 'prefer-dark'
+org.cinnamon.desktop.interface cursor-theme 'DMZ-White'
+# gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark'
 ### Panel Layout: Traditional
 gsettings set org.cinnamon app-menu-label 'Menu'
 gsettings set org.cinnamon panels-height "['1:27']"
-gsettings set org.cinnamon enabled-applets "['panel1:left:0:menu@cinnamon.org:30', 'panel1:left:1:show-desktop@cinnamon.org:31', 'panel1:left:2:panel-launchers@cinnamon.org:32', 'panel1:left:3:window-list@cinnamon.org:33', 'panel1:right:0:systray@cinnamon.org:34', 'panel1:right:1:xapp-status@cinnamon.org:35', 'panel1:right:2:keyboard@cinnamon.org:36', 'panel1:right:3:notifications@cinnamon.org:37', 'panel1:right:4:printers@cinnamon.org:38', 'panel1:right:5:removable-drives@cinnamon.org:39', 'panel1:right:6:favorites@cinnamon.org:40', 'panel1:right:7:user@cinnamon.org:41', 'panel1:right:8:network@cinnamon.org:42', 'panel1:right:9:sound@cinnamon.org:43', 'panel1:right:10:power@cinnamon.org:44', 'panel1:right:11:calendar@cinnamon.org:45']"
 gsettings set org.cinnamon panel-zone-icon-sizes '[{"panelId": 1, "left": 0, "center": 0, "right": 0}]'
-gsettings set org.cinnamon next-applet-id 46
+# gsettings set org.cinnamon enabled-applets "['panel1:left:0:menu@cinnamon.org:30', 'panel1:left:1:show-desktop@cinnamon.org:31', 'panel1:left:2:panel-launchers@cinnamon.org:32', 'panel1:left:3:window-list@cinnamon.org:33', 'panel1:right:0:systray@cinnamon.org:34', 'panel1:right:1:xapp-status@cinnamon.org:35', 'panel1:right:2:keyboard@cinnamon.org:36', 'panel1:right:3:notifications@cinnamon.org:37', 'panel1:right:4:printers@cinnamon.org:38', 'panel1:right:5:removable-drives@cinnamon.org:39', 'panel1:right:6:favorites@cinnamon.org:40', 'panel1:right:7:user@cinnamon.org:41', 'panel1:right:8:network@cinnamon.org:42', 'panel1:right:9:sound@cinnamon.org:43', 'panel1:right:10:power@cinnamon.org:44', 'panel1:right:11:calendar@cinnamon.org:45']"
+# gsettings set org.cinnamon next-applet-id 46
 ### Background image
-gsettings set org.cinnamon.desktop.background picture-uri 'file:///usr/share/backgrounds/linuxmint-una/acoelho_lush.jpg'
+gsettings set org.cinnamon.desktop.background picture-uri 'file:///usr/share/backgrounds/linuxmint-vera/aburden_grass.jpg'
 
 ##### In Settings.
 # Privacy:
 gsettings set org.cinnamon.desktop.privacy remember-recent-files false
 
 ### Disable effects
-gsettings set org.cinnamon.muffin desktop-effects false
 gsettings set org.cinnamon startup-animation false
 gsettings set org.cinnamon desktop-effects-workspace false
 gsettings set org.cinnamon enable-vfade false
@@ -140,7 +144,8 @@ gsettings set org.cinnamon enable-vfade false
 ##### Add applet
 # Places Center
 
-gsettings set org.cinnamon enabled-applets "['panel1:left:0:menu@cinnamon.org:78', 'panel1:right:12:show-desktop@cinnamon.org:79', 'panel1:left:2:panel-launchers@cinnamon.org:80', 'panel1:left:4:window-list@cinnamon.org:81', 'panel1:right:1:systray@cinnamon.org:82', 'panel1:right:2:xapp-status@cinnamon.org:83', 'panel1:right:3:keyboard@cinnamon.org:84', 'panel1:right:4:notifications@cinnamon.org:85', 'panel1:right:5:printers@cinnamon.org:86', 'panel1:right:6:removable-drives@cinnamon.org:87', 'panel1:right:7:favorites@cinnamon.org:88', 'panel1:right:8:user@cinnamon.org:89', 'panel1:right:9:network@cinnamon.org:90', 'panel1:right:10:sound@cinnamon.org:91', 'panel1:right:11:power@cinnamon.org:92', 'panel1:right:12:calendar@cinnamon.org:93', 'panel1:left:3:placesCenter@scollins:94']"
+gsettings set org.cinnamon enabled-applets "['panel1:left:0:menu@cinnamon.org:78', 'panel1:right:12:show-desktop@cinnamon.org:79', 'panel1:left:2:panel-launchers@cinnamon.org:80', 'panel1:left:4:window-list@cinnamon.org:81', 'panel1:right:1:systray@cinnamon.org:82', 'panel1:right:2:xapp-status@cinnamon.org:83', 'panel1:right:3:keyboard@cinnamon.org:84', 'panel1:right:4:notifications@cinnamon.org:85', 'panel1:right:5:printers@cinnamon.org:86', 'panel1:right:7:favorites@cinnamon.org:88', 'panel1:right:8:user@cinnamon.org:89', 'panel1:right:9:network@cinnamon.org:90', 'panel1:right:10:sound@cinnamon.org:91', 'panel1:right:11:power@cinnamon.org:92', 'panel1:right:12:calendar@cinnamon.org:93', 'panel1:left:3:placesCenter@scollins:94']"
+
 gsettings set org.cinnamon next-applet-id 95
 
 ##### List view
@@ -159,7 +164,6 @@ gsettings set org.nemo.desktop computer-icon-visible false
 gsettings set org.nemo.desktop volumes-visible false
 gsettings set org.nemo.desktop home-icon-visible false
 
-
 ### Sound
 gsettings set org.cinnamon.desktop.sound volume-sound-enabled false
 gsettings set org.cinnamon.sounds plug-enabled false
@@ -175,6 +179,12 @@ gsettings set org.cinnamon favorite-apps "['firefox.desktop']"
 # Ignored System reports
 gsettings set com.linuxmint.report ignored-reports "['install-language-packs', 'install-media-codecs']"
 
+# Remove from home
+cd
+rmdir Documents/ Music/ Pictures/ Public/ Public/ Templates/ Videos/
+
+# Customize Menu: TODO
+# vi  ~/.config/cinnamon/spices/menu@cinnamon.org/*.json
 
 ##### Updates and install
 sudo apt update && sudo apt upgrade -y
